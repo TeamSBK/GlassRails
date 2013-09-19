6a66861160c6c43c73499b5a7208a22e38be6075
Adrian Peterson Co <adrianpco@gmail.com>
2013-09-19 15:42:06 +0800

    Work SON OF A BITCH!

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module GlassRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.js *.css)
+    config.assets.precompile += %w(*.js)
   end
 end
